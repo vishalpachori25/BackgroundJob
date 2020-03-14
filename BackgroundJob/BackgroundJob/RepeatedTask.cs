98de8c6b2c6ca5d7ce393c5b45f8c3bf43dc1ff5
Vishal Pachori <vishalpachori25@gmail.com>
2020-03-14 20:20:50 +0530

    Task modified

--- BackgroundJob/BackgroundJob/RepeatedTask.cs
+++ BackgroundJob/BackgroundJob/RepeatedTask.cs
@@ -8,31 +8,55 @@ namespace BackgroundJob
 {
     public class RepeatedTask : IJob
     {
+        const string SAMPLE_URL = "HTTPS//SOME.SAMPLE.URL";
         public async Task<bool> Run(JobInfo jobInfo, CancellationToken cancelToken)
         {
-            var client = new RestClient("https://kidsandfun.site/wp-admin/admin-ajax.php")
+            //if cancellation is requested then do nothing
+            if (cancelToken.IsCancellationRequested)
             {
-                Timeout = -1
-            };
-            
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("User-Agent", "Mozilla/4.0 (Macintosh; Intel Mac OS X 10.13; rv:71.0) Gecko/20100101 Firefox/"+ jobInfo.PeriodicTime?.Seconds);
-            request.AddHeader("Accept", "*/*");
-            request.AddHeader("Accept-Language", "en-US,en;q=0.5");
-            request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-            request.AddHeader("X-Requested-With", "XMLHttpRequest");
-            request.AddHeader("Origin", "https://kidsandfun.site");
-            request.AddHeader("DNT", "1");
-            request.AddHeader("Connection", "keep-alive");
-            request.AddHeader("Referer", "https://kidsandfun.site/february-2020/?contest=photo-detail&photo_id=1119");
-            request.AddHeader("TE", "Trailers");
-            request.AddHeader("Cookie", "4");
-            request.AddParameter("application/x-www-form-urlencoded; charset=UTF-8", "action=vote_for_photo&photo_id=1119&nonce_id=02802094c7&option_id=basicaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",  ParameterType.RequestBody);
-            IRestResponse response = await client.ExecuteAsync(request);
-            Console.WriteLine("response :-"+response.Content);
+                return false;
+            }
+            // Logic when job should run
+            var runJob = false;
+            // if you want to run the job only two times a day or at specific time you can do that
+            DateTime dateTime9 = DateTime.Today;
+            dateTime9 = dateTime9.AddHours(9);
+
+            DateTime dateTime4 = DateTime.Today;
+            dateTime4 = dateTime4.AddHours(15);
+            // Jobinfo provides when the job is last ran
+            if (jobInfo.LastRunUtc == null) // job has never run
+            {
+                runJob = true;
+            }
+            else if ((jobInfo.LastRunUtc?.ToLocalTime().CompareTo(dateTime4)) >= 0)
+            {
+                runJob = true;  // its been at least 4 o'clcock since the last run
+
+            }
+            else if ((jobInfo.LastRunUtc?.ToLocalTime().CompareTo(dateTime9)) >= 0)
+            {
+                runJob = true;
+            }
+
+            if (runJob)
+            {
+                //Your task 
+                // you can also inject your service/ dependency in the Job
+                var client = new RestClient(SAMPLE_URL);
+
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("User-Agent", "Mozilla/4.0 (Macintosh; Intel Mac OS X 10.13; rv:71.0) Gecko/20100101 Firefox/" + jobInfo.PeriodicTime?.Seconds);
+                request.AddHeader("Accept", "*/*");
+                request.AddHeader("Accept-Language", "en-US,en;q=0.5");
+                request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                IRestResponse response = await client.ExecuteAsync(request);
+                Console.WriteLine("response :-" + response.Content);
 
 
-            return true;
+                return true;
+            }
+            return false;
         }
     }
 }
